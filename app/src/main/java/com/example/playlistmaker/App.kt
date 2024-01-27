88d053ba44b721c7389d2a2646f6c2c3b030b443
Козлова Елена <lastonetwo18@gmail.com>
2024-01-27 13:52:55 +0300

    Theme switcher

---
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -0,0 +1,38 @@
+package com.example.playlistmaker
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class App : Application() {
+
+    private var darkTheme = false
+
+    private val sharedPrefs by lazy {
+        getSharedPreferences(SHARED_PREFERENCE_NAME, MODE_PRIVATE)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        darkTheme = sharedPrefs.getBoolean(THIEME_SWITCH_KEY, false)
+        sharedPrefs.edit()
+            .putBoolean(THIEME_SWITCH_KEY, darkTheme)
+            .apply()
+        switchTheme(darkTheme)
+    }
+
+    fun switchTheme(darkThemeEnabled: Boolean) {
+        darkTheme = darkThemeEnabled
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkThemeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
+
+    companion object {
+        const val THIEME_SWITCH_KEY = "THIEME_SWITCH_KEY"
+        const val SHARED_PREFERENCE_NAME = "PLAYLISTMAKER_SHARED_PREFS"
+    }
+}
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,14 @@ class SettingsActivity : AppCompatActivity() {
         setShareBtnListener()
         setSupportBtnListener()
         setTermsOfUseBtnListener()
+        setThemeSwitcherListener()
+    }
+
+    private fun setThemeSwitcherListener() {
+        val themeSwitcher = findViewById<SwitchMaterial>(R.id.themeSwitcher)
+        themeSwitcher.setOnCheckedChangeListener { switcher, checked ->
+            (applicationContext as App).switchTheme(checked)
+        }
     }
 
     private fun setBackBtnListener() {
