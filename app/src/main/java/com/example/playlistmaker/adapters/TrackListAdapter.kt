91609b25b9837ffd5dca7f684ee746d52ccbef16
Козлова Елена <lastonetwo18@gmail.com>
2024-03-02 14:28:13 +0300

    Sprint 14 - add click debounce

--- app/src/main/java/com/example/playlistmaker/adapters/TrackListAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/adapters/TrackListAdapter.kt
@@ -2,6 +2,8 @@ package com.example.playlistmaker.adapters
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Handler
+import android.os.Looper
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -16,17 +18,27 @@ class TrackListAdapter(
     private val pref: SharedPreferences
 ) : RecyclerView.Adapter<TrackViewHolder> () {
 
+    companion object {
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
+    }
+
+    private var isClickAllowed = true
+
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = TrackViewHolder(parent)
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         val item = trackList[position]
 
         holder.itemView.setOnClickListener {
-            writeToHistory(item)
+            if (clickDebounce()) {
+                writeToHistory(item)
 
-            val intent = Intent(holder.itemView.context, TrackActivity::class.java)
-            intent.putExtra(TrackActivity.BUNDLE_KEY_TRACK, item)
-            holder.itemView.context.startActivity(intent)
+                val intent = Intent(holder.itemView.context, TrackActivity::class.java)
+                intent.putExtra(TrackActivity.BUNDLE_KEY_TRACK, item)
+                holder.itemView.context.startActivity(intent)
+            }
         }
 
         holder.bind(item)
@@ -39,6 +51,17 @@ class TrackListAdapter(
     private fun writeToHistory(track: ITunesTrack) {
         val history = SearchHistory(pref)
         history.write(track)
+    }
+
+    private fun clickDebounce(): Boolean {
+        val current = isClickAllowed
+
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true}, CLICK_DEBOUNCE_DELAY)
+        }
+
+        return current
     }
 
 }
